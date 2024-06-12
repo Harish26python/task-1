8c1f640f66c927e53d6725ead90d22fced67dee6
Harish26python <rayharish2003@gmail.com>
2024-06-12 23:15:35 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,18 @@
+# List Operations
+print("List Operations:")
+my_list = [1, 2, 3, 4, 5]
+print("Original List:", my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("After appending 6:", my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("After removing 3:", my_list)
+
+# Modifying an element in the list
+my_list[2] = 10
+print("After modifying the element at index 2:", my_list)
+print()
+
---
+++ set.py
@@ -0,0 +1,14 @@
+# Set Operations
+print("Set Operations:")
+my_set = {1, 2, 3, 4, 5}
+print("Original Set:", my_set)
+
+# Adding an element to the set
+my_set.add(6)
+print("After adding 6:", my_set)
+
+# Removing an element from the set
+my_set.remove(3)
+print("After removing 3:", my_set)
+
+
